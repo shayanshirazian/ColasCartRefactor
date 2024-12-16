7a8c09e1bec7d213fb49cdb2c8d34c309db4dcdc
shayan shirazian <shayan@colaskitchen.com>
2024-12-16 17:11:07 +0330

    upcoming orders main cart added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,58 @@
-import React from "react";
-import TabSwitch from "./cartDetails/TabSwitch";
+"use client";
+import React, { useState, useEffect } from "react";
+import "../../app/api/dogs/route";
+import TabsSwitch from "./cartDetails/TabSwitch";
+import UpcomingOrders from "./upcoming/upcomingOrders"
+import UpcomingOrdersModal from "./upcoming/upcomingOrdersModal";
 
 const Profile = () => {
+  const [orders, setOrders] = useState([]);
+  if (!orders) {
+    return <div>Loading...</div>;
+  }
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isUpcomingOrdersModalOpen, setUpcomingOrdersModalOpen] =
+    useState(false);
+  const [selectedOrder, setSelectedOrder] = useState({});
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch("/api/dogs");
+        if (!response.ok) {
+          throw new Error("Failed to fetch orders");
+        }
+        const data = await response.json();
+        setOrders(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchOrders();
+  }, []);
+
+  const handleOpenModal = (order) => {
+    if (order) {
+      setSelectedOrder(order);
+      setModalOpen(true);
+    } else {
+      console.error("Invalid order passed to handleOpenModal");
+    }
+  };
+
+  const handleCloseUpcomingModal = () => {
+    setUpcomingOrdersModalOpen(false);
+  };
+
+  if (loading) return <p className="text-center mt-4">Loading Orders...</p>;
+  if (error) return <p className="text-center text-red-500 mt-4">{error}</p>;
+
+  const shippedOrders = orders.filter((order) => order.status === "Shipped");
+
   return (
     <div className="MainContainer flex items-center bg-green justify-center mt-10 min-h-screen w-full bg-green-50">
       <div className="InnerContainer flex flex-col  p-5 bg-gray-100 border border-gray-300 rounded-3xl w-full max-w-3xl">
@@ -9,9 +60,24 @@ const Profile = () => {
           <button className="backBtn text-green-700 m-5">
             <img width="20px" src="/left-arrow.svg" alt="back btn" />
           </button>
-          <TabSwitch />
+          <TabsSwitch />
+        </div>
+
+        <div className="Upcoming Orders text-gray-600 mt-3 ml-1">
+          <UpcomingOrders
+            orders={shippedOrders}
+            setModalOpen={setUpcomingOrdersModalOpen}
+          />
         </div>
       </div>
+      <UpcomingOrdersModal
+        orders={shippedOrders}
+        isOpen={isUpcomingOrdersModalOpen}
+        onOpenModal={handleOpenModal}
+        onClose={handleCloseUpcomingModal}
+      />
+
+     
     </div>
   );
 };
---
+++ app/profile/upcoming/upcomingOrders.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const UpcomingOrders = ({ orders, setModalOpen }) => {
+  if (!orders) {
+    return null;
+  }
+  return (
+    <div aria-label="Upcoming Orders">
+      <div className="text-gray-600 mt-3 ml-1">
+        {`Upcoming Orders`}
+        <div
+          aria-label="UpcomingStyle"
+          className=" flex flex-col m-3 p-4 rounded-lg shadow-md border border-grey-200"
+        >
+          <div aria-label="dataContainer" className=" flex justify-between">
+            <div aria-label="Left">
+              <div>
+                <span className="font-bold">{orders?.[0]?.deliveryDate}</span>
+                <span className="text-xs text-gray-500 ml-3">
+                  {orders?.[0]?.recipes}
+                </span>
+                <span className="bg-blue-100 text-blue-500 text-xs rounded-full px-3 py-1 ml-3">
+                  {orders?.[0]?.status}
+                </span>
+                <div>
+                  <div className="text-gray-400 mt-2">
+                    <span>For: </span>
+                    <span className="text-gray-500 text-xs mt-2">
+                      {orders.map((dog) => dog.dogName).join(", ")}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div aria-label="Right flex">
+              <button
+                className="text-lg font-extrabold text-green-700"
+                onClick={() => {
+                  setModalOpen(true);
+                }}
+              >
+                {`>`}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpcomingOrders;
